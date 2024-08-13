83745f37b2299a63245130f2e48165bc8fda4f2c
Aleksey Fedorenko <shintio.afedorenko@gmail.com>
2024-08-14 01:55:48 +0300

    Add Shintio.MachineTranslation

---
+++ src/MachineTranslation/Shintio.MachineTranslation.Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shintio.MachineTranslation.Abstractions;
+using Shintio.MachineTranslation.GoogleApi;
+
+namespace Shintio.MachineTranslation.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddTranslation(this IServiceCollection services)
+	{
+		return services.AddSingleton<ITranslator, GoogleTranslator>();
+	}
+}
---
+++ src/MachineTranslation/Shintio.MachineTranslation.GoogleApi/GoogleTranslator.cs
@@ -0,0 +1,34 @@
+﻿using GTranslatorAPI;
+using ITranslator = Shintio.MachineTranslation.Abstractions.ITranslator;
+
+namespace Shintio.MachineTranslation.GoogleApi;
+
+public class GoogleTranslator : ITranslator
+{
+	private readonly Translator _translator;
+
+	public GoogleTranslator()
+	{
+		_translator = new Translator();
+	}
+
+	public async Task<string?> TranslateAsync(string text, string fromLanguage, string toLanguage)
+	{
+		try
+		{
+			var result = await _translator.TranslateAsync(
+				fromLanguage,
+				toLanguage,
+				text
+			);
+
+			return result?.TranslatedText;
+		}
+		catch
+		{
+			// ignored
+		}
+
+		return null;
+	}
+}
