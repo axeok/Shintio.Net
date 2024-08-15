927cfce5581747563fb29ef8287c20e112061e3c
Aleksey Fedorenko <shintio.afedorenko@gmail.com>
2024-08-15 18:33:25 +0300

    Add Wrap to AutoProxyService

--- src/Misc/Shintio.Web/Services/AutoProxyService.cs
+++ src/Misc/Shintio.Web/Services/AutoProxyService.cs
@@ -1,29 +1,64 @@
-﻿using Shintio.Web.Interfaces;
+﻿using Microsoft.Extensions.Logging;
+using Shintio.Web.Interfaces;
 using Shintio.Web.Utils;
 
 namespace Shintio.Web.Services;
 
 public class AutoProxyService
 {
+	private readonly ILogger<AutoProxyService> _logger;
 	private readonly IProxyProvider _proxyProvider;
 	private readonly IHttpClientFactory _factory;
 	private readonly AutoProxyHttpClientHandler _handler;
 
-	public AutoProxyService(IHttpClientFactory factory, IProxyProvider proxyProvider, AutoProxyHttpClientHandler handler)
+	private HttpClient? _client;
+
+	public AutoProxyService(
+		ILogger<AutoProxyService> logger,
+		IHttpClientFactory factory,
+		IProxyProvider proxyProvider,
+		AutoProxyHttpClientHandler handler
+	)
 	{
+		_logger = logger;
 		_factory = factory;
 		_proxyProvider = proxyProvider;
 		_handler = handler;
 	}
 
-	public HttpClient GetClient()
-	{
-		return _factory.CreateClient(nameof(AutoProxyService));
-	}
+	public HttpClient Client => UpdateClient();
 
 	public async Task NewProxy(bool removeCurrent)
 	{
 		await _proxyProvider.RescanProxiesAsync(removeCurrent);
 		_handler.UpdateProxy();
+		UpdateClient();
+	}
+
+	public async Task<HttpResponseMessage> Wrap(Func<HttpClient, Task<HttpResponseMessage>> sendRequest)
+	{
+		try
+		{
+			return await sendRequest(Client);
+		}
+		catch (HttpRequestException exception)
+		{
+			_logger.LogWarning(exception, "Exception in wrap");
+
+			if (exception.HttpRequestError != HttpRequestError.ConnectionError)
+			{
+				throw;
+			}
+
+			await NewProxy(true);
+			UpdateClient();
+
+			return await Wrap(sendRequest);
+		}
+	}
+
+	private HttpClient UpdateClient()
+	{
+		return _client = _factory.CreateClient(nameof(AutoProxyService));
 	}
 }
