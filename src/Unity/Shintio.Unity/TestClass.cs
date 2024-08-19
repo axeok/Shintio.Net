f428e4a2e692afd734fa1360cf1661107b7b3699
Aleksey Fedorenko <shintio.afedorenko@gmail.com>
2024-08-19 22:46:04 +0300

    Add unity files

--- src/Unity/Shintio.Unity/TestClass.cs
+++ src/Unity/Shintio.Unity/TestClass.cs
@@ -1,10 +1,12 @@
-﻿namespace Shintio.Unity
+﻿using Shintio.Json.Interfaces;
+
+namespace Shintio.Unity
 {
 	public class TestClass
 	{
 		public static string Test()
 		{
-			return "Hello from Shintio.Net";
+			return $"Hello from Shintio.Net {typeof(IJson).FullName}";
 		}
 	}
 }
