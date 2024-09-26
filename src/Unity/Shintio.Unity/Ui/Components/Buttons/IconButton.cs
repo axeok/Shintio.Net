f76098d6adc1ae3a2d4cf1f14ab88bbf94b6fdef
Aleksey Fedorenko <shintio.afedorenko@gmail.com>
2024-09-27 01:22:52 +0300

    Add navbar

---
+++ src/Unity/Shintio.Unity/Ui/Components/Buttons/IconButton.cs
@@ -0,0 +1,28 @@
+﻿using Shintio.Unity.Ui.Components.Icons;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Shintio.Unity.Ui.Components.Buttons
+{
+	[RequireComponent(typeof(Button))]
+	public class IconButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+	{
+		private SmartToggleIcon _icon = null!;
+
+		private void Start()
+		{
+			_icon = GetComponentInChildren<SmartToggleIcon>();
+		}
+
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			_icon.Toggle.Value = false;
+		}
+
+		public void OnPointerUp(PointerEventData eventData)
+		{
+			_icon.Toggle.Value = true;
+		}
+	}
+}
---
+++ src/Unity/Shintio.Unity/Ui/Components/Icons/SmartIcon.cs
@@ -0,0 +1,52 @@
+﻿using Cysharp.Threading.Tasks;
+using R3;
+using Shintio.Unity.Utils;
+using Unity.VectorGraphics;
+using UnityEngine;
+
+namespace Shintio.Unity.Ui.Components.Icons
+{
+	[ExecuteInEditMode]
+	[RequireComponent(typeof(SVGImage))]
+	public class SmartIcon : MonoBehaviour
+	{
+		private SVGImage _svgImage = null!;
+
+		private string _oldIcon = "";
+
+		public SerializableReactiveProperty<string> IconName = new();
+
+		private void Awake()
+		{
+			_svgImage = GetComponent<SVGImage>();
+
+			IconName.Subscribe(_ => TryUpdateIcon());
+		}
+
+#if UNITY_EDITOR
+		private async UniTaskVoid Update()
+		{
+			await TryUpdateIcon();
+		}
+#endif
+
+		private async UniTask TryUpdateIcon()
+		{
+			if (_oldIcon == IconName.Value)
+			{
+				return;
+			}
+
+			_oldIcon = IconName.Value;
+
+			var sprite = await Iconify.LoadIcon(IconName.Value);
+			if (!sprite)
+			{
+				return;
+			}
+
+			_svgImage ??= GetComponent<SVGImage>();
+			_svgImage.sprite = sprite;
+		}
+	}
+}
